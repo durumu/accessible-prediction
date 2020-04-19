1566d9fd4c55b492f1f2bcc9a218946cdbda6d76
Amsal Kassam <aakassam1@yahoo.com>
2020-04-19 16:47:58 -0500

    Refactored mostlikely.py
    Removed main() and replaced it with prediction(history) which will take the history as an argument and return an array of characters predicted

--- mostlikely.py
+++ mostlikely.py
@@ -4,8 +4,9 @@ from string import ascii_lowercase
 MAX_LOOKBACK = 4
 LEARN_RATE = 0.05
 
-wiki_counts = defaultdict(lambda: [0]*27)
-user_counts = defaultdict(lambda: [0]*27)
+wiki_counts = defaultdict(lambda: [0] * 27)
+user_counts = defaultdict(lambda: [0] * 27)
+
 
 def index(letter):
     if letter.isalpha():
@@ -17,7 +18,7 @@ def index(letter):
 def priority(prediction, next_letter):
     wiki_count = wiki_counts[prediction][index(next_letter)]
     user_count = user_counts[prediction][index(next_letter)]
-    return max(wiki_count, 1) * (1 + LEARN_RATE * user_count**1.5)
+    return max(wiki_count, 1) * (1 + LEARN_RATE * user_count ** 1.5)
 
 
 def next_by_priority(prefix):
@@ -25,26 +26,29 @@ def next_by_priority(prefix):
         prefix = prefix[-MAX_LOOKBACK:]
 
     letters = ascii_lowercase + '_'
+
     def sort_key(letter):
         return [priority(prefix[start:], letter) for start in range(len(prefix))]
-    
-    return sorted(letters, key = sort_key, reverse = True)
+
+    return sorted(letters, key=sort_key, reverse=True)
 
 
 def initialize_wiki_counts():
     with open('freqs.dat') as f:
         for line in f:
             word, count = line.split('|')
-            count = int(count) # we do nothing with count right now
+            count = int(count)  # we do nothing with count right now
             prefix, next_letter = word[:-1], word[-1]
             wiki_counts[prefix][index(next_letter)] = count
 
+
 def update_user_counts(history):
     for lookback in range(1, MAX_LOOKBACK):
-        prefix = ''.join(history[-lookback-1:-1])
+        prefix = ''.join(history[-lookback - 1:-1])
         user_counts[prefix][index(history[-1])] += 1
 
-def main():
+
+"""def main():
     initialize_wiki_counts()
 
     history = [' ']
@@ -58,8 +62,16 @@ def main():
         inputted_character = input()[0]
         history.append(inputted_character)
         
+        update_user_counts(history)"""
+
+
+def predictions(history):
+    while True:
+        lookback = min(MAX_LOOKBACK, len(history))
+
+        recent = ''.join(history[-lookback:])
         update_user_counts(history)
+        return next_by_priority(recent)
 
 
-if __name__ == "__main__":
-    main()
+initialize_wiki_counts()
